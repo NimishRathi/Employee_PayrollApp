a2980ad0e8618b12e170c06b0d08a1ed97f5ca99
NimishRathi <nimish.rathi2002@gmail.com>
2025-02-27 11:16:40 +0530

    [nimish]add.UC2_PayrollApp

---
+++ UC2_PayrollApp/src/main/java/com/capgemini/UC2_PayrollApp/Uc2PayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.UC2_PayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc2PayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc2PayrollAppApplication.class, args);
+	}
+
+}

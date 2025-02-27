a081cbdcb27881938adcec957f5f6a7b58fe1fbd
NimishRathi <nimish.rathi2002@gmail.com>
2025-02-27 11:18:53 +0530

    [nimish]add.UC4_PayrollApp

---
+++ UC4_PayrollApp/src/main/java/com/capgemini/UC4_PayrollApp/Uc4PayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.UC4_PayrollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc4PayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc4PayrollAppApplication.class, args);
+	}
+
+}
